1e74cb3e72e9ea4fa99826ad15751c12f69ca68d
Alex Betancourt <alex@Alexs-MacBook-Pro.local>
2021-12-05 23:16:56 -0500

    added function to create future dates and added new function to forecastStats function

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,7 @@ var runSearch = function () {
       currentStats(data);
       var lon = data.coord.lon;
       var lat = data.coord.lat;
-      fetch (requestUrl5Days(lat, lon))
+      fetch(requestUrl5Days(lat, lon))
         .then(function (response) {
           return response.json();
         })
@@ -25,8 +25,8 @@ var runSearch = function () {
           UVI(data);
           forecastDivEl(fiveDayForecast);
           forecastAhead(data);
-        })
-    })
+        });
+    });
 };
 // functions within runSearch
 // gets the current day url
@@ -46,8 +46,8 @@ var requestUrl5Days = function (lat, lon) {
     "&lon=" +
     lon +
     "&exclude=minutely,hourly,alerts&units=imperial&cnt=5&appid=01cf9d1edee20eca447b27b51d4fdc55";
-    console.log(requestUrl5Days);
-    return requestUrl5Days;
+  console.log(requestUrl5Days);
+  return requestUrl5Days;
 };
 
 // creates search history button
@@ -85,9 +85,9 @@ var checkButton = function (listItemCity) {
 };
 
 // creates div for current day
-var infoDivEl = function(currentDay) {
+var infoDivEl = function (currentDay) {
   var currentDayHeader = document.createElement("h2");
-  currentDayHeader.textContent= "Current Day Forecast";
+  currentDayHeader.textContent = "Current Day Forecast";
   currentDay[0].appendChild(currentDayHeader);
   var infoDivEl = document.createElement("div");
   infoDivEl.className = "info";
@@ -102,13 +102,25 @@ var currentDate = function () {
   var currentDate = new Date();
   var date =
     " (" +
-    (currentDate.getMonth() +1) +
+    (currentDate.getMonth() + 1) +
     "/" +
     currentDate.getDate() +
     "/" +
     currentDate.getFullYear() +
     ")";
   return date;
+};
+
+// get dates for forecasted weather
+var forecastedDates = function ([i]) {
+  var currentDate = new Date();
+  var forecastedDate =
+    currentDate.getMonth() + 1 +
+    "/" +
+    (currentDate.getDate() + i + 1) +
+    "/" +
+    currentDate.getFullYear();
+    return forecastedDate;
 };
 
 // gets current stats and appends them to current day div
@@ -123,7 +135,7 @@ var currentStats = function (data) {
   var img = document.createElement("img");
   img.src =
     "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
-    console.log(img);
+  console.log(img);
   currentDayList.appendChild(img);
   // Temperature
   var currentTemperatureLI = document.createElement("li");
@@ -139,86 +151,102 @@ var currentStats = function (data) {
   currentDayList.appendChild(currentWindSpeedLI);
 };
 
-// gets the UVI to then append to current day div 
+// gets the UVI to then append to current day div
 var UVI = function (data) {
   var currentDayList = document.getElementById("current-day-info");
   var uviLI = document.createElement("li");
   uviLI.textContent = "UV Index: " + data.current.uvi;
-  var colorIndex = function (){
-  if (data.current.uvi < 3) {
-    uviLI.className = "green";
-  } else if (data.current.uvi < 6) {
-    uviLI.className = "yellow";
-  } else if (data.current.uvi < 9) {
-    uviLI.className = "orange";
-  } else if (data.current.uvi < 11) {
-    uviLI.className = "red";
-  } else if (data.current.uvi >= 11) {
-    uviLI.className = "violet";
-  }
-}
-colorIndex();
-currentDayList.appendChild(uviLI);
-}
+  var colorIndex = function () {
+    if (data.current.uvi < 3) {
+      uviLI.className = "green";
+    } else if (data.current.uvi < 6) {
+      uviLI.className = "yellow";
+    } else if (data.current.uvi < 9) {
+      uviLI.className = "orange";
+    } else if (data.current.uvi < 11) {
+      uviLI.className = "red";
+    } else if (data.current.uvi >= 11) {
+      uviLI.className = "violet";
+    }
+  };
+  colorIndex();
+  currentDayList.appendChild(uviLI);
+};
 
 // creates div for 5 day forecast
 var forecastDivEl = function (fiveDayForecast) {
   var forecastHeader = document.createElement("h2");
-  forecastHeader.textContent= "5-Day Forecast";
+  forecastHeader.textContent = "5-Day Forecast";
   fiveDayForecast[0].appendChild(forecastHeader);
-  var forecastDiv = document.createElement("div")
-  forecastDiv.className = "row info"
-  fiveDayForecast[0].appendChild(forecastDiv)
-}
+  var forecastDiv = document.createElement("div");
+  forecastDiv.className = "row info";
+  fiveDayForecast[0].appendChild(forecastDiv);
+};
 
 // loops through array of 5 days and runs forecastStats()
 var forecastAhead = function (data) {
-  var forecastedDays = [[data.daily[1]],[data.daily[2]],[data.daily[3]],[data.daily[4]],[data.daily[5]]];
-  for (i=0; i<forecastedDays.length; i++) {
-  forecastDayDivs([i]); 
-  forecastStats(forecastedDays, [i]);
+  var forecastedDays = [
+    [data.daily[1]],
+    [data.daily[2]],
+    [data.daily[3]],
+    [data.daily[4]],
+    [data.daily[5]],
+  ];
+  for (i = 0; i < forecastedDays.length; i++) {
+    forecastDayDivs([i]);
+    forecastStats(forecastedDays, [i]);
   }
 };
 
 // creates divs for the next 5 days
 var forecastDayDivs = function ([i]) {
   var forecastDiv = document.getElementsByClassName("row info");
-  var boxEl = document.createElement("div")
-  boxEl.className = "forecastBoxEl col-4 info"
-  boxEl.id = "day-"+[i];
+  var boxEl = document.createElement("div");
+  boxEl.className = "forecastBoxEl col-4 info";
+  boxEl.id = "day-" + [i];
   forecastDiv[0].appendChild(boxEl);
   var ulEl = document.createElement("ul");
-  ulEl.className = "forecast-ul day-" +[i];
+  ulEl.className = "forecast-ul day-" + [i];
   boxEl.appendChild(ulEl);
 };
 
 //gets 5 day forecast and appends them to each div
 var forecastStats = function (forecastedDays, [i]) {
-var forecastUl = document.getElementsByClassName("day-" + [i]);
+  var forecastUl = document.getElementsByClassName("day-" + [i]);
 
-// Weather Icon
-var forecastimg = document.createElement("img");
-forecastimg.src =
-  "http://openweathermap.org/img/wn/" + forecastedDays[i][0].weather[0].icon + "@2x.png";
-console.log(forecastimg);
-forecastUl[0].appendChild(forecastimg);
+  // Dates
+  forecastedDates([i]);
+  var futureDate = document.createElement("li");
+  futureDate.textContent = forecastedDates([i]);
+  forecastUl[0].appendChild(futureDate);
 
-// Temperature
-var forecastTempLi = document.createElement("li");
-forecastTempLi.textContent = "Temp: " + forecastedDays[i][0].temp.day + " F";
-forecastUl[0].appendChild(forecastTempLi);
+  // Weather Icon
+  var forecastimg = document.createElement("img");
+  forecastimg.src =
+    "http://openweathermap.org/img/wn/" +
+    forecastedDays[i][0].weather[0].icon +
+    "@2x.png";
+  console.log(forecastimg);
+  forecastUl[0].appendChild(forecastimg);
 
-// Humidity
-var forecastHumidityLi = document.createElement("li");
-forecastHumidityLi.textContent = "Humidity: " + forecastedDays[i][0].humidity + "%";
-forecastUl[0].appendChild(forecastHumidityLi);
+  // Temperature
+  var forecastTempLi = document.createElement("li");
+  forecastTempLi.textContent = "Temp: " + forecastedDays[i][0].temp.day + " F";
+  forecastUl[0].appendChild(forecastTempLi);
 
-// Wind Speed
-var forecastWindSpeedLi = document.createElement("li");
-forecastWindSpeedLi.textContent = "Wind: " + forecastedDays[i][0].wind_speed + " MPH";
-forecastUl[0].appendChild(forecastWindSpeedLi);
+  // Humidity
+  var forecastHumidityLi = document.createElement("li");
+  forecastHumidityLi.textContent =
+    "Humidity: " + forecastedDays[i][0].humidity + "%";
+  forecastUl[0].appendChild(forecastHumidityLi);
+
+  // Wind Speed
+  var forecastWindSpeedLi = document.createElement("li");
+  forecastWindSpeedLi.textContent =
+    "Wind: " + forecastedDays[i][0].wind_speed + " MPH";
+  forecastUl[0].appendChild(forecastWindSpeedLi);
 };
-// runs any button clicked on the side menu 
+// runs any button clicked on the side menu
 var runSearchHistoryItem = function (event) {
   var locationRequested = event.target.id;
   document.getElementById("search-bar").value = locationRequested;
@@ -228,7 +256,7 @@ var runSearchHistoryItem = function (event) {
 // clears the search bar after searching and deletes information for prior search
 var clearInfo = function () {
   var selectedAreaDay = document.getElementById("current");
-  var selectedAreaForecast = document.getElementById("forecast")
+  var selectedAreaForecast = document.getElementById("forecast");
   selectedAreaDay.textContent = "";
   selectedAreaForecast.textContent = "";
 };
